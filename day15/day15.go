0f7c7948777503ed3fa70da725222cee05578029
Alexander Wolden <awolden@squareup.com>
2021-12-15 17:39:50 -0800

    day 15 sucks

--- day15/day15.go
+++ day15/day15.go
@@ -20,11 +20,18 @@ type Point struct {
 }
 
 type Path struct {
-	points []Point
-	total  int
+	curX    int
+	curY    int
+	total   int
+	nextVal int
 }
 
-var memo = map[string]int{}
+func (path Path) Hash() string {
+	return strconv.Itoa(path.curX) + "," + strconv.Itoa(path.curY)
+}
+
+var pathVals = map[string]int{}
+var visited = map[string]bool{}
 var genMap = map[string]int{}
 var best = 1<<(UintSize-1) - 1
 var achievedEnd = false
@@ -33,7 +40,12 @@ func main() {
 	input := readInput()
 	newInput := multiplyArray(input)
 	//fmt.Println(len(newInput), len(newInput[0]))
-	takePath(newInput, 0, 0, 0)
+	takePath(newInput, Path{
+		curX:    0,
+		curY:    0,
+		total:   0,
+		nextVal: 0,
+	})
 	// fmt.Println()
 	// for i := 0; i < len(newInput); i++ {
 	// 	for j := 0; j < len(newInput[0]); j++ {
@@ -47,45 +59,92 @@ func main() {
 
 var hasBest = false
 
-func takePath(input [][]int, curX int, curY int, currentTotal int) {
-	maxX := (len(input[0]) - 1)
-	maxY := (len(input) - 1)
-	if v, ok := memo[strconv.Itoa(curX)+","+strconv.Itoa(curY)]; ok {
-		if v < currentTotal {
-			// fmt.Println("bailing early cause we've been here", strconv.Itoa(curX)+","+strconv.Itoa(curY))
-			return
+func checkIfIsBetter(path Path) bool {
+	//fmt.Println("mem", memo)
+
+	if v, ok := pathVals[path.Hash()]; ok {
+		if v <= path.total && v != 0 {
+			//fmt.Println("bailing early cause we've been here", strconv.Itoa(path.curX)+","+strconv.Itoa(path.curY), memo, v, ok)
+			return false
 		} else {
-			memo[strconv.Itoa(curX)+","+strconv.Itoa(curY)] = currentTotal
+			pathVals[path.Hash()] = path.total
 		}
 	} else {
-		memo[strconv.Itoa(curX)+","+strconv.Itoa(curY)] = currentTotal
+		pathVals[path.Hash()] = path.total
+	}
+	return true
+}
+
+func takePath(input [][]int, path Path) {
+	maxX := (len(input[0]) - 1)
+	maxY := (len(input) - 1)
+
+	queue := []Path{path}
+
+	i := 0
+	for len(queue) > 0 {
+		i++
+		if i%100000 == 0 {
+			fmt.Println("i:", i)
+		}
+		//fmt.Println("visited", queue)
+
+		path := queue[0]
+		queue = queue[1:]
+		//fmt.Println("path", path.Hash(), visited[path.Hash()])
+		visited[path.Hash()] = true
+
+		curX := path.curX
+		curY := path.curY
+		currentTotal := path.total
+
+		if currentTotal < best && curX == maxX && curY == maxY {
+			best = currentTotal
+			hasBest = true
+			fmt.Println("new best", best)
+			achievedEnd = true
+			continue
+		}
+
+		nextPaths := []Path{}
+
+		if curY < maxY {
+			nextPaths = append(nextPaths, Path{curX: curX, curY: curY + 1, total: pathVals[path.Hash()] + input[curY+1][curX], nextVal: input[curY+1][curX]})
+			//takePath(input, curX, curY+1, currentTotal+input[curY+1][curX])
+		}
+		if curX < maxX {
+			nextPaths = append(nextPaths, Path{curX: curX + 1, curY: curY, total: pathVals[path.Hash()] + input[curY][curX+1], nextVal: input[curY][curX+1]})
+			//takePath(input, curX+1, curY, currentTotal+input[curY][curX+1])
+		}
+		if curX > 0 {
+			nextPaths = append(nextPaths, Path{curX: curX - 1, curY: curY, total: pathVals[path.Hash()] + input[curY][curX-1], nextVal: input[curY][curX-1]})
+			//takePath(input, curX-1, curY, currentTotal+input[curY][curX-1])
+		}
+		if curY > 0 {
+			nextPaths = append(nextPaths, Path{curX: curX, curY: curY - 1, total: pathVals[path.Hash()] + input[curY-1][curX], nextVal: input[curY-1][curX]})
+			// takePath(input, curX, curY-1, currentTotal+input[curY-1][curX])
+		}
+
+		//fmt.Println("next path", nextPaths)
+		for _, nextPath := range nextPaths {
+			checkIfIsBetter(nextPath)
+			if !visited[nextPath.Hash()] && !ContainsPath(queue, nextPath) {
+				//fmt.Println("appending path", nextPath.Hash())
+				queue = append(queue, nextPath)
+			}
+		}
+
+	}
+}
+
+func ContainsPath(s []Path, i Path) bool {
+	for _, v := range s {
+		if v.Hash() == i.Hash() {
+			return true
+		}
 	}
 
-	if hasBest && currentTotal > best {
-		return
-	}
-
-	if currentTotal < best && curX == maxX && curY == maxY {
-		best = currentTotal
-		hasBest = true
-		fmt.Println("new best", best)
-		achievedEnd = true
-		return
-	}
-
-	if curY < maxY {
-		takePath(input, curX, curY+1, currentTotal+input[curY+1][curX])
-	}
-	if curX < maxX {
-		takePath(input, curX+1, curY, currentTotal+input[curY][curX+1])
-	}
-	if curX > 0 {
-		takePath(input, curX-1, curY, currentTotal+input[curY][curX-1])
-	}
-	if curY > 0 {
-		takePath(input, curX, curY-1, currentTotal+input[curY-1][curX])
-	}
-
+	return false
 }
 
 func multiplyArray(foo [][]int) [][]int {
